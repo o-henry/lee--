0d79831cfc1c14aefd15c1520a0884e3db61ff82
Henry <c.henry.9209@gmail.com>
2024-11-02 20:00:30 +0900

    [FEAT] : player ui 수정중

--- src/components/bento/Player.tsx
+++ src/components/bento/Player.tsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect, useRef } from 'react'
+import { Skeleton } from '@/components/ui/skeleton'
 
 const Player = () => {
   const audioRef = useRef<HTMLAudioElement | null>(null)
@@ -7,13 +8,14 @@ const Player = () => {
   const [timelinePosition, setTimelinePosition] = useState(0)
   const [isMuted, setIsMuted] = useState(false)
 
+  // mx-auto mt-8 grid max-w-[800px] grid-cols-3 gap-5 px-4
   // 오디오 파일 목록
   const playlist = [
+    '/static/music/enchanted-chimes-177906.mp3',
     '/static/music/christmas-party-time-249613.mp3',
     '/static/music/deck-the-halls-background-christmas-music-for-video-calesta-version-253224.mp3',
     '/static/music/christmas-short-v1-254790.mp3',
     '/static/music/we-wish-you-a-merry-christmas-bells-background-xmas-music-for-video-256126.mp3',
-    '/static/music/enchanted-chimas-177906.mp3',
     '/static/music/cozy-christmas-eve-251739.mp3',
   ]
 
@@ -83,138 +85,151 @@ const Player = () => {
   }
 
   return (
-    <div className="audio-player">
+    <div>
       <audio ref={audioRef}></audio>
-      <div className="controls">
-        <button onClick={handlePrev} className="player-button-left">
-          {/* 이전 트랙 아이콘 */}◀︎
-        </button>
+      {/* 레이어 */}
+      <div></div>
 
-        <button onClick={handlePlayPause} className="player-button h-4 w-4">
-          {isPlaying ? (
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 20 20"
-              fill="#FFFFFF"
-              className="h-6 w-6"
-            >
-              <path
-                fillRule="evenodd"
-                d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zM7 8a1 1 0 012 0v4a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v4a1 1 0 102 0V8a1 1 0 00-1-1z"
-                clipRule="evenodd"
-              />
-            </svg>
-          ) : (
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 20 20"
-              fill="#FFFFFF"
-              className="h-6 w-6"
-            >
-              <path
-                fillRule="evenodd"
-                d="M10 18a8 8 0 100-16 8 8 0 000 16zM9.555 7.168A1 1 0 008 8v4a1 1 0 001.555.832l3-2a1 1 0 000-1.664l-3-2z"
-                clipRule="evenodd"
-              />
-            </svg>
-          )}
-        </button>
-
-        <button onClick={handleNext} className="player-button-right">
-          {/* 다음 트랙 아이콘 */}▶
-        </button>
-
-        <input
-          type="range"
-          className="timeline"
-          value={timelinePosition}
-          onChange={handleTimelineChange}
-          max="100"
-        />
-
-        <button onClick={handleMuteToggle} className="sound-button">
-          {isMuted ? (
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 20 20"
-              fill="#FFFFFF"
-              className="h-4 w-4"
-            >
-              <path
-                fillRule="evenodd"
-                d="M9.383 3.076A1 1 0 0110 4v12a1 1 0 01-1.707.707L4.586 13H2a1 1 0 01-1-1V8a1 1 0 011-1h2.586l3.707-3.707a1 1 0 011.09-.217zM12.293 7.293a1 1 0 011.414 0L15 8.586l1.293-1.293a1 1 0 111.414 1.414L16.414 10l1.293 1.293a1 1 0 01-1.414 1.414L15 11.414l-1.293 1.293a1 1 0 01-1.414-1.414L13.586 10l-1.293-1.293a1 1 0 010-1.414z"
-                clipRule="evenodd"
-              />
-            </svg>
-          ) : (
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 20 20"
-              fill="#FFFFFF"
-              className="h-4 w-4"
-            >
-              <path
-                fillRule="evenodd"
-                d="M9.383 3.076A1 1 0 0110 4v12a1 1 0 01-1.707.707L4.586 13H2a1 1 0 01-1-1V8a1 1 0 011-1h2.586l3.707-3.707a1 1 0 011.09-.217zM14.657 2.929a1 1 0 011.414 0A9.972 9.972 0 0119 10a9.972 9.972 0 01-2.929 7.071 1 1 0 01-1.414-1.414A7.971 7.971 0 0017 10c0-2.21-.894-4.208-2.343-5.657a1 1 0 010-1.414zm-2.829 2.828a1 1 0 011.415 0A5.983 5.983 0 0115 10a5.984 5.984 0 01-1.757 4.243 1 1 0 01-1.415-1.415A3.984 3.984 0 0013 10a3.983 3.983 0 00-1.172-2.828 1 1 0 010-1.415z"
-                clipRule="evenodd"
-              />
-            </svg>
-          )}
-        </button>
-      </div>
-      <main className="grid min-h-screen place-items-center from-blue-200 to-indigo-900">
+      <main className="">
         <div>
-          <section className="gap-4">
-            <div className="rounded-sm bg-gray-900 p-3 shadow-lg">
-              <div className="group relative">
-                /// TODO
-                <div className="absolute top-0 flex h-full w-full items-center justify-evenly rounded bg-black bg-opacity-0 transition group-hover:bg-opacity-60 group-hover:opacity-100">
-                  <button className="translate-y-3 transform text-white opacity-0 transition hover:scale-110 group-hover:translate-y-0 group-hover:opacity-100">
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      width="20"
-                      height="20"
-                      fill="currentColor"
-                      className="bi bi-heart"
-                      viewBox="0 0 16 16"
-                    >
-                      <path d="M8 2.748l-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01L8 2.748zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143c.06.055.119.112.176.171a3.12 3.12 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15z" />
-                    </svg>
-                  </button>
+          {/* <section className="gap-4"> */}
+          {/* <div className="rounded bg-gray-800 p-3 shadow-lg"> */}
+          <div className="relative flex h-full w-full flex-col justify-between p-6">
+            <img
+              className="w-[55%] rounded-xl border-border"
+              src="/assets/winter_bg/brooke-cagle-sg4Mw88fe_U-unsplash.jpg"
+              alt=""
+            />
 
-                  <button className="translate-y-3 transform text-white opacity-0 transition hover:scale-110 group-hover:translate-y-0 group-hover:opacity-100">
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      width="40"
-                      height="40"
-                      fill="currentColor"
-                      className="bi bi-play-circle-fill"
-                      viewBox="0 0 16 16"
-                    >
-                      <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM6.79 5.093A.5.5 0 0 0 6 5.5v5a.5.5 0 0 0 .79.407l3.5-2.5a.5.5 0 0 0 0-.814l-3.5-2.5z" />
-                    </svg>
-                  </button>
+            {/* <div className="absolute top-0 flex h-full w-full items-center justify-evenly rounded bg-black bg-opacity-0 transition group-hover:bg-opacity-60 group-hover:opacity-100"> */}
+            <div className="flex min-w-0 flex-1 flex-col justify-end overflow-hidden">
+              <div className="mt-6 flex items-center justify-center">
+                <button
+                  onClick={handlePrev}
+                  className="rounded-full bg-white p-3 hover:bg-gray-300 focus:outline-none"
+                >
+                  <svg
+                    width="64px"
+                    height="64px"
+                    viewBox="0 0 24 24"
+                    className="h-4 w-4 text-gray-600"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                    transform="matrix(-1, 0, 0, 1, 0, 0)"
+                  >
+                    <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+                    <g
+                      id="SVGRepo_tracerCarrier"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                    ></g>
+                    <g id="SVGRepo_iconCarrier">
+                      <path
+                        d="M16.6598 14.6474C18.4467 13.4935 18.4467 10.5065 16.6598 9.35258L5.87083 2.38548C4.13419 1.26402 2 2.72368 2 5.0329V18.9671C2 21.2763 4.13419 22.736 5.87083 21.6145L16.6598 14.6474Z"
+                        fill="#000000"
+                      ></path>
+                      <path
+                        d="M22.75 5C22.75 4.58579 22.4142 4.25 22 4.25C21.5858 4.25 21.25 4.58579 21.25 5V19C21.25 19.4142 21.5858 19.75 22 19.75C22.4142 19.75 22.75 19.4142 22.75 19V5Z"
+                        fill="#000000"
+                      ></path>
+                    </g>
+                  </svg>
+                </button>
 
-                  <button className="translate-y-3 transform text-white opacity-0 transition hover:scale-110 group-hover:translate-y-0 group-hover:opacity-100">
+                <button
+                  onClick={handlePlayPause}
+                  className="mx-4 rounded-full bg-white p-4 hover:bg-gray-300 focus:outline-none"
+                >
+                  {isPlaying ? (
                     <svg
+                      width="64px"
+                      height="64px"
+                      viewBox="0 0 24 24"
+                      className="h-6 w-6 text-gray-600"
+                      fill="#FFFFFF"
                       xmlns="http://www.w3.org/2000/svg"
-                      width="20"
-                      height="20"
-                      fill="currentColor"
-                      className="bi bi-three-dots"
-                      viewBox="0 0 16 16"
                     >
-                      <path d="M3 9.5a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3zm5 0a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3zm5 0a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3z" />
+                      <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+                      <g
+                        id="SVGRepo_tracerCarrier"
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                      ></g>
+                      <g id="SVGRepo_iconCarrier">
+                        <path
+                          d="M2 6C2 4.11438 2 3.17157 2.58579 2.58579C3.17157 2 4.11438 2 6 2C7.88562 2 8.82843 2 9.41421 2.58579C10 3.17157 10 4.11438 10 6V18C10 19.8856 10 20.8284 9.41421 21.4142C8.82843 22 7.88562 22 6 22C4.11438 22 3.17157 22 2.58579 21.4142C2 20.8284 2 19.8856 2 18V6Z"
+                          fill="#000000"
+                        ></path>
+                        <path
+                          d="M14 6C14 4.11438 14 3.17157 14.5858 2.58579C15.1716 2 16.1144 2 18 2C19.8856 2 20.8284 2 21.4142 2.58579C22 3.17157 22 4.11438 22 6V18C22 19.8856 22 20.8284 21.4142 21.4142C20.8284 22 19.8856 22 18 22C16.1144 22 15.1716 22 14.5858 21.4142C14 20.8284 14 19.8856 14 18V6Z"
+                          fill="#000000"
+                        ></path>
+                      </g>
                     </svg>
-                  </button>
-                </div>
-              </div>
-              <div className="p-5">
-                <h3 className="text-lg text-white">Epoch</h3>
-                <p className="text-gray-400">Tycho</p>
+                  ) : (
+                    <svg
+                      width="64px"
+                      height="64px"
+                      viewBox="0 0 24 24"
+                      fill="none"
+                      className="h-6 w-6 text-black"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        d="M8.286 3.407A1.5 1.5 0 0 0 6 4.684v14.632a1.5 1.5 0 0 0 2.286 1.277l11.888-7.316a1.5 1.5 0 0 0 0-2.555L8.286 3.407z"
+                        fill="#000000"
+                      />
+                    </svg>
+                  )}
+                </button>
+
+                <button
+                  onClick={handleNext}
+                  className="rounded-full bg-white p-3 hover:bg-gray-300 focus:outline-none"
+                >
+                  <svg
+                    width="64px"
+                    height="64px"
+                    viewBox="0 0 24 24"
+                    className="h-4 w-4 text-gray-600"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+                    <g
+                      id="SVGRepo_tracerCarrier"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                    ></g>
+                    <g id="SVGRepo_iconCarrier">
+                      <path
+                        d="M16.6598 14.6474C18.4467 13.4935 18.4467 10.5065 16.6598 9.35258L5.87083 2.38548C4.13419 1.26402 2 2.72368 2 5.0329V18.9671C2 21.2763 4.13419 22.736 5.87083 21.6145L16.6598 14.6474Z"
+                        fill="#000000"
+                      ></path>
+                      <path
+                        d="M22.75 5C22.75 4.58579 22.4142 4.25 22 4.25C21.5858 4.25 21.25 4.58579 21.25 5V19C21.25 19.4142 21.5858 19.75 22 19.75C22.4142 19.75 22.75 19.4142 22.75 19V5Z"
+                        fill="#000000"
+                      ></path>
+                    </g>
+                  </svg>
+                </button>
               </div>
             </div>
-          </section>
+          </div>
+
+          <div className="pt-1">
+            {/* <input
+                  type="range"
+                  className="timeline"
+                  value={timelinePosition}
+                  onChange={handleTimelineChange}
+                  max="100"
+                /> */}
+            {/* <h3 className="pt-1 text-center text-lg text-white">playing</h3> */}
+            {/* <p className="text-gray-400">PLAYING</p> */}
+          </div>
+          {/* </div> */}
+          {/* </section> */}
         </div>
       </main>
     </div>
