d072994782bfbf77a180a464ae4a28f4ffe791bf
Henry <c.henry.9209@gmail.com>
2024-11-20 15:08:30 +0900

    [UPDATE] : UI 20241120

--- src/consts.ts
+++ src/consts.ts
@@ -22,7 +22,7 @@ export const SITE: Site = {
 }
 
 export const NAV_LINKS: Link[] = [
-  { href: '/blog', label: 'blog' },
+  { href: '/blog', label: 'archive' },
   // { href: '/authors', label: 'authors' },
   { href: '/about', label: 'about' },
   // { href: '/tags', label: 'tags' },
